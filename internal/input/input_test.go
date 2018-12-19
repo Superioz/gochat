9b89b021cd27be6590eec5c674f34dee0875b30f
Tobias B <germansuperioz@yahoo.de>
2018-12-19 21:20:39 +0100

    Added input test

---
+++ internal/input/input_test.go
@@ -0,0 +1,24 @@
+package input
+
+import "testing"
+
+func TestCommand_IsValid(t *testing.T) {
+	var raw Input = "label some arguments"
+	var raw2 Input = "label"
+	var raw3 Input = ""
+
+	cmd := raw.AsCommand()
+	if !cmd.IsValid() || len(cmd.Arguments) != 2 {
+		t.Error("command not valid")
+	}
+
+	cmd2 := raw2.AsCommand()
+	if !cmd2.IsValid() {
+		t.Error("command not valid")
+	}
+
+	cmd3 := raw3.AsCommand()
+	if cmd3.IsValid() {
+		t.Error("command valid despite invalidation expected")
+	}
+}
